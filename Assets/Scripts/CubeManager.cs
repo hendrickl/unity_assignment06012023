b02102107a02236ec5b1882ee79731194e83d94a
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-06 16:30:51 +0100

    Object spawns on mouse position

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _prefab;
+    private Vector3 _clickPosition;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InstantiateObj();
+    }
+
+    void InstantiateObj()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            _clickPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 2.0f));
+            GameObject.Instantiate(_prefab, _clickPosition, Quaternion.identity);
+        }
     }
 }
