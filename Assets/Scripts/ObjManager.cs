4e5169815552b7670588ee5980a5f493d106c4b6
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-09 15:25:03 +0100

    Fix position on new spawned object

--- Assets/Scripts/ObjManager.cs
+++ Assets/Scripts/ObjManager.cs
@@ -6,51 +6,38 @@ using UnityEngine;
 
 public class ObjManager : MonoBehaviour
 {
-    private Vector3 _clickPosition;
+    private Vector3 _mousePosition;
     private int _numberOfObj = 0;
-    private bool _canInstantiate = true;
+    private float _distanceToCam = 10f;
+    private List<GameObject> _prefabs = new List<GameObject>();
 
-    [SerializeField] private GameObject _prefab;
-    [SerializeField] private List<GameObject> _prefabs = new List<GameObject>();
-
-    void Start()
-    {
-    }
+    [SerializeField] private GameObject _objToSpawn;
 
     // Update is called once per frame
     void Update()
     {
-        InstantiateObj();
-        // InstantiateObjOnNumberMax();
+        InstantiateObjAtMousePosition();
     }
 
     // Instantiate object at mouse position and on click
-    void InstantiateObj()
+    void InstantiateObjAtMousePosition()
     {
-        _clickPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.0f));
+        _mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distanceToCam));
 
         if (Input.GetKeyUp(KeyCode.Mouse0) && _numberOfObj < 10)
         {
-            GameObject.Instantiate(_prefab, _clickPosition, Quaternion.identity);
-
-            Debug.Log(_numberOfObj + " objects instantiated");
-
+            GameObject _prefab = Instantiate(_objToSpawn, _mousePosition, Quaternion.identity);
             _numberOfObj++;
-
             _prefabs.Add(_prefab);
             Debug.Log(String.Join(", ", _prefabs));
-
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0) && _numberOfObj > 9)
         {
-            for (int i = 0; i < 1; i++)
-            {
-                GameObject.Instantiate(_prefabs[i], _clickPosition, Quaternion.identity);
-                Debug.Log("POOL triggered at " + _prefabs[i]);
-                _prefabs.Remove(_prefabs[i]);
-
-            }
+            GameObject newobjectspawned = _prefabs[0];
+            newobjectspawned.transform.position = _mousePosition;
+            _prefabs.RemoveAt(0);
+            _prefabs.Add(newobjectspawned);
         }
     }
 }
